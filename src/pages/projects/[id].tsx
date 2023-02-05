b72d80a5522ef5e082dfc07393f3d0b809d0e7e0
Bee <xronsredstone@gmail.com>
2023-02-04 22:57:39 -0500

    ugly resize

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -5,6 +5,7 @@ import Navbar from "../../Components/navbar";
 import Footer from "../../Components/footer";
 import projectsData from "../../../public/projects.json";
 
+
 const ProjectPage = () => {
   const idString = useRouter().query.id as string;
   const project = projectsData.find((item) => item.name === idString);
@@ -17,10 +18,10 @@ const ProjectPage = () => {
   React.useEffect(() => {
     if (!project) return;
     setPageHeader(project.name);
-    project.pageContent ? setPageContent(project.pageContent) : setPageContent(" ");
-    project.image ? setPageImage(project.image) : setPageImage("https://developer.valvesoftware.com/w/images/5/5b/Missing_textures_example.png");
+    project.pageContent ? setPageContent(project.pageContent) : setPageContent("");
     project.githubLink ? setGithubLink(project.githubLink) : setGithubLink("https://github.com/MadeOfBees");
     project.deployedLink ? setDeployedLink(project.deployedLink) : null;
+    setPageImage(`/images/${project.image}`);
   }, [project]);
 
   return (
