966b3ba10a7685121703bf5850d2c272636ccab8
Bee <xronsredstone@gmail.com>
2023-03-05 16:52:18 -0500

    navData

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -8,17 +8,22 @@ interface NavbarProps {
 export default function Navbar({ without }: NavbarProps) {
   const [projectNames, setProjectNames] = React.useState([]);
   const [newProjectNames, setNewProjectNames] = React.useState([]);
-
+  
+  const getPages = async (): Promise<void> => {
+    const response = await fetch("/api/projects/");
+    const data = await response.json();
+    if (data && data.projects) {
+      const pageNames = data.projects.map((project: { name: string }) => project.name);
+      const newPageNames = data.projects
+        .filter((project: { new: boolean }) => project.new === true)
+        .map((project: { name: string }) => project.name);
+      setProjectNames(pageNames);
+      setNewProjectNames(newPageNames);
+    }
+  };
+  
   React.useEffect(() => {
-    fetch("/projects.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const newProjects = data.filter((item: { new: boolean }) => item.new);
-        setProjectNames(data.map((item: { name: string }) => item.name));
-        setNewProjectNames(
-          newProjects.map((item: { name: string }) => item.name)
-        );
-      });
+    getPages();
   }, []);
 
   return (
