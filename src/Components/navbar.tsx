6c7d0e81e6aa71cf97a8cb45876a5c480d3a6d31
Bee <xronsredstone@gmail.com>
2023-03-05 17:25:49 -0500

    time to add to database

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -8,26 +8,29 @@ interface NavbarProps {
 export default function Navbar({ without }: NavbarProps) {
   const [projectNames, setProjectNames] = React.useState([]);
   const [newProjectNames, setNewProjectNames] = React.useState([]);
-  
+
   const getPages = async (): Promise<void> => {
     const response = await fetch("/api/projects/");
     const data = await response.json();
     if (data && data.projects) {
-      const pageNames = data.projects.map((project: { name: string }) => project.name);
+      const pageNames = data.projects.map(
+        (project: { name: string }) => project.name
+      );
       const newPageNames = data.projects
         .filter((project: { new: boolean }) => project.new === true)
         .map((project: { name: string }) => project.name);
       setProjectNames(pageNames);
       setNewProjectNames(newPageNames);
+      console.log("pageNames", pageNames);
     }
   };
-  
+
   React.useEffect(() => {
     getPages();
   }, []);
 
   return (
-    <nav className="btm-nav">
+    <nav className="btm-nav bg-base-300">
       {without !== "home" ? (
         <button>
           <Link href="/">
@@ -39,11 +42,22 @@ export default function Navbar({ without }: NavbarProps) {
           <span className="btm-nav-label">Home</span>
         </button>
       )}
-      <button>
-        <span className="btm-nav-label">Projects</span>
+      <button className="dropdown dropdown-top">
+        <label className="btm-nav-label">Projects</label>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-200 w-full">
+          {projectNames.map((projectName: string) => (
+            <li key={projectName}>
+              <Link href={`/projects/${projectName}`}>
+                <p className="menu-item">{projectName}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </button>
       <button>
-        <span className="btm-nav-label">Contact Me</span>
+        <label htmlFor="contactModal">
+          <span className="btm-nav-label">Contact</span>
+        </label>
       </button>
     </nav>
   );
