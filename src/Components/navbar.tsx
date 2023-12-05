1a24c2a3e9ff2a0cfc016735781c7285d8d36ed0
Bee <xronsredstone@gmail.com>
2023-12-05 18:31:39 -0500

    aboutMe

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,7 +1,6 @@
-"use client";
 import React from "react";
 import { LuMailPlus, LuMenu } from "react-icons/lu";
-import { useState, useEffect, Fragment } from "react";
+import { useState, Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 
 export default function navbar() {
@@ -17,54 +16,81 @@ export default function navbar() {
   };
 
   return (
-    <header className="flex justify-between items-center mx-auto p-4 text-xl ml-4 bg-white h-[4.7rem]">
-      <nav className="hidden md:block items-center">
-        <button className="mr-8">Logo</button>
-        <button className={underlineMainRoute(0)}>Home</button>
-        <button className={underlineMainRoute(1)}>About</button>
-        <Menu>
-          <Menu.Button className={underlineMainRoute(2)}>Projects</Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute top-[3.6rem] w-[9rem] bg-white shadow focus:outline-none border border-[#313445] border-opacity-20">
-              {projectsArray.map((project) => (
-                <Menu.Item as="div" key={project}>
-                  {project}
-                </Menu.Item>
-              ))}
+    <header className="h-[4.7rem]">
+      <div className="flex justify-between items-center mx-auto p-4 text-xl fixed w-full z-20 top-0 start-0 bg-white border-b border-[#000000] border-opacity-20 h-[4.7rem]">
+        <nav className="hidden md:block items-center ml-4">
+          <button className="mr-8 ml-12">Logo</button>
+          <button className={underlineMainRoute(0)}>Home</button>
+          <button className={underlineMainRoute(1)}>About</button>
+          <Menu>
+            <Menu.Button className={underlineMainRoute(2)}>
+              Projects
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute top-[3.6rem] left-[18.2rem] w-[9rem] bg-white shadow focus:outline-none border border-[#313445] border-opacity-20">
+                {projectsArray.map((project) => (
+                  <Menu.Item as="div" key={project}>
+                    {project}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </nav>
+        <div className="md:hidden ml-4">
+          <Menu>
+            <Menu.Button className="flex ring-2 ring-[#000000] rounded h-[2.2rem] w-[2.2rem] justify-center items-center">
+              <LuMenu />
+            </Menu.Button>
+            <Menu.Items className="absolute top-[3.6rem] w-[9rem] rounded-sm bg-white shadow focus:outline-none border border-[#313445] border-opacity-20">
+              <Menu.Item as="div" key="Home">
+                Home
+              </Menu.Item>
+              <Menu.Item as="div" key="About">
+                About
+              </Menu.Item>
+              <Menu.Items as="div" key="Projects">
+                <Menu>
+                  <Menu.Button className="focus:outline-none">
+                    Projects
+                  </Menu.Button>
+                  <Menu.Items className="">
+                    {projectsArray.map((project) => (
+                      <Menu.Item as="div" key={project}>
+                        {project}
+                      </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Menu>
+              </Menu.Items>
             </Menu.Items>
-          </Transition>
-        </Menu>
-      </nav>
-      <div className="md:hidden">
-        <button className="ring-2 ring-[#000000] rounded p-1">
-            <LuMenu />
-        </button>
-      </div>
-      <div className="md:hidden">
-        <button>Logo</button>
-      </div>
-      <div className="items-center space-x-6 rtl:space-x-revers flex md:order-2">
-        <div className="items-center space-x-1 rtl:space-x-reverse md:hidden mr-4">
-          <button className="flex items-center justify-center bg-[#313445] hover:bg-[#42465d] text-white rounded h-[2.2rem] w-[2.2rem]">
-            <LuMailPlus />
-          </button>
+          </Menu>
         </div>
-        <div className="relative hidden md:block space-x-1 rtl:space-x-reverse">
-          <button className="mr-12 flex items-center justify-center bg-[#313445] hover:bg-[#42465d] text-white px-3 rounded h-[2.2rem] w-[11.5rem] text-md">
-            <LuMailPlus className="mr-2" />
-            Work with me
-          </button>
+        <div className="md:hidden">
+          <button>Logo</button>
+        </div>
+        <div className="items-center space-x-6 rtl:space-x-revers flex md:order-2">
+          <div className="items-center space-x-1 rtl:space-x-reverse md:hidden mr-4">
+            <button className="flex items-center justify-center bg-[#313445] hover:bg-[#42465d] text-white rounded h-[2.2rem] w-[2.2rem]">
+              <LuMailPlus />
+            </button>
+          </div>
+          <div className="relative hidden md:block space-x-1 rtl:space-x-reverse">
+            <button className="mr-12 flex items-center justify-center bg-[#313445] hover:bg-[#42465d] text-white px-3 rounded h-[2.2rem] w-[11.5rem] text-[1.125rem]">
+              <LuMailPlus className="mr-2" />
+              Work with me
+            </button>
+          </div>
         </div>
       </div>
     </header>
   );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,10 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import NavBar from "@/Components/navbar";
+import { LuMailPlus } from "react-icons/lu";
 
 export default function Home() {
   const [email, setEmail] = useState("");
-  function greetings() {
-    if (new Date().getHours() < 12) {
-      return "morning";
-    } else if (new Date().getHours() < 18) {
-      return "afternoon";
-    } else {
-      return "evening";
-    }
-  }
 
   const sendEmail = () => {
     window.open(`mailto:isaud@gmail.com?subject=NewContact!&body=${email}`);
@@ -29,12 +21,85 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="w-[100vw]">
         <NavBar />
-        <div className="bg-[#87A878] h-[33rem]">About</div>
-        <div className="bg-[#272A38] h-[33rem]">My Tech Stack</div>
-        <div className="bg-white h-[25rem]">Projects</div>
-        <div className="bg-[#272A38] h-[33rem]">Contact</div>
+        <div className="bg-[#87A878] px-[2rem] min-h-[33rem]">
+          <div className="py-[4rem] md:px-[6.3rem] grid grid-cols-3">
+            <div className="col-start-1 col-span-2 flex-col space-y-[4rem] flex justify-center my-auto">
+              <h1 className="text-5xl font-bold text-white">Hi, I'm Bee.</h1>
+              <h2 className="text-xl font-semibold text-white">
+                A Full Stack Web Developer with a flair for turning complex
+                problems into elegant, user-friendly solutions. My journey in
+                tech is peppered with unique experiences that fuel my innovative
+                approach to web development.
+              </h2>
+              <button className="flex items-center justify-center bg-[#313445] hover:bg-[#42465d] text-white px-3 rounded h-[2.2rem] w-[11.5rem] text-[1.125rem]">
+                <LuMailPlus className="mr-2" />
+                Work with me
+              </button>
+            </div>
+            <div className="col-start-3 col-span-1">
+              <div className="hidden md:block">
+                <img src="/images/pfp.png" alt="Photo of Bee" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#272A38] flex flex-col text-white justify-center items-stretch">
+          <div className="justify-center px-16 py-12 max-md:px-5">
+            <div className="text-center text-5xl font-bold max-md:text-4xl ">
+              My tech stack
+            </div>
+            <div className="text-center text-xl leading-8 mt-14 max-md:mt-10">
+              Proficient in Next.js, Tailwind CSS, TypeScript, and PHP, I craft
+              web experiences that are both elegant and efficient. My game
+              development projects, including DailySudoku and ReactJack,
+              highlight my creativity and technical skills.
+            </div>
+            <div className="mt-12 max-md:max-w-full max-md:mt-10">
+              <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+                <div className="flex flex-col items-stretch w-[34%] max-md:w-full max-md:ml-0">
+                  <div className="items-stretch flex flex-col pr-2 max-md:mt-10">
+                    <div className="text-2xl font-bold leading-8">
+                      Front-end
+                    </div>
+                    <div className="leading-7 mt-5">
+                      Lorem ipsum dolor sit amet consectetur. Porta at lectus
+                      leo eget condimentum dui aliquam. Amet nulla.
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-stretch w-[34%] max-md:w-full max-md:ml-0">
+                  <div className="items-stretch flex flex-col pr-2 max-md:mt-10">
+                    <div className="text-2xl font-bold leading-8">Back-end</div>
+                    <div className="leading-7 mt-5">
+                      Lorem ipsum dolor sit amet consectetur. Porta at lectus
+                      leo eget condimentum dui aliquam. Amet nulla.
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-stretch w-[34%] max-md:w-full max-md:ml-0">
+                  <div className="items-stretch flex flex-col pr-2 max-md:mt-10">
+                    <div className="text-2xl font-bold leading-8">
+                      Game Development
+                    </div>
+                    <div className="leading-7 mt-5">
+                      Lorem ipsum dolor sit amet consectetur. Porta at lectus
+                      leo eget condimentum dui aliquam. Amet nulla.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="items-start flex max-w-full justify-between gap-5 mt-8 max-md:flex-wrap max-md:justify-center">
+              <p>Img1</p>
+              <p>Img2</p>
+              <p>Img3</p>
+              <p>Img4</p>
+              <p>Img5</p>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
