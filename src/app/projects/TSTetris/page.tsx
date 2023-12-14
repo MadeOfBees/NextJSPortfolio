4eefef87c97dc282658207300c22db5a0c0f9124
Bee <xronsredstone@gmail.com>
2023-12-13 19:32:40 -0500

    tired of figuring out scaling, just using breakpoints, not what I wanted but its what I deserve

--- src/app/projects/TSTetris/page.tsx
+++ src/app/projects/TSTetris/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import NavBar from "@/Components/navbar";
 import ContactModal from "@/Components/contactModal";
+import ProjectsCarousel from "@/Components/projectsCarousel";
 import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function TSTetrisProjectPage() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,16 +12,28 @@ export default function TSTetrisProjectPage() {
     <main className="w-[100vw] relative">
       <NavBar setIsOpen={setIsOpen} currentSearch="/projects/TSTetris" />
 
-      <div className="bg-[#87A878] h-[33rem] justify-start">
-        <h1 className="text-5xl sm:text-6xl font-bold text-white">
-        TS Tetris</h1>
+      <div className="absolute right-0 left-0 m-auto bg-[#122f49] top-[15rem] h-[40rem] w-[80rem]"></div>
+
+      <div className="bg-[#87A878] h-[40rem] justify-start md:pl-[7rem] pl-[5rem] pt-[4.4rem]">
+        <h1 className="text-5xl sm:text-6xl font-bold text-white">TSTetris</h1>
+      </div>
+      <div className="bg-white flex flex-col text-black h-[40rem] justify-center items-center">
+        <div className="">
+          <p className="text-2xl sm:text-3xl mt-4 font-light text-center w-[60rem]">
+            Lorem ipsum dolor sit amet consectetur. Eget faucibus accumsan purus
+            id senectus. Eu ut nullam massa massa sit vestibulum faucibus sapien
+            purus. Cursus risus ullamcorper augue consectetur. Amet placerat
+            aliquam id ut mus eleifend nibh donec vulputate. Ultrices quam
+            venenatis faucibus cras aliquam velit scelerisque.
+          </p>
+          <div className="flex flex-row justify-between mt-8">
+            {/* div for the github button */}
+            <a className="flex flex-row items-center"></a>
+          </div>
+        </div>
       </div>
 
-      <div className="absolute top-[20rem] left-0 right-0 mx-auto w-[50rem] h-[20rem] bg-red-500 rounded-lg"></div>
-
-
-      <div className="bg-white px-[4.9rem] py-[4.4rem] h-[33rem] justify-start"></div>
-
+      <ProjectsCarousel currentSearch="TSTetris" />
       <ContactModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </main>
   );
