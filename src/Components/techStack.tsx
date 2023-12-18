a9af5b4573760820d40f0df956a25e40c0b025ce
Bee <xronsredstone@gmail.com>
2023-12-18 12:23:18 -0500

    techStackIcons

--- src/Components/techStack.tsx
+++ src/Components/techStack.tsx
@@ -1,6 +1,24 @@
 "use client";
 import { useState } from "react";
 
+import { SiNextdotjs } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaPhp } from "react-icons/fa";
+
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
+import { FaLinux } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+import { SiUnrealengine } from "react-icons/si";
+import { SiAdobe } from "react-icons/si";
+import { SiBlender } from "react-icons/si";
+import { SiUnity } from "react-icons/si";
+import { FaAndroid } from "react-icons/fa";
+
 export default function TechStack() {
   type techStack = {
     name: string;
@@ -12,20 +30,32 @@ export default function TechStack() {
     {
       name: "Front-end",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Porta at lectus leo eget condimentum dui aliquam. Amet nulla.",
-      images: ["img1", "img2", "img3", "img4", "img5"],
+        "With a focus on designing fast and responsive web apps, I use Next.js, Tailwind CSS, and TypeScript to create elegant and efficient user experiences.",
+      images: [
+        "NextJS",
+        "React",
+        "TailwindCSS",
+        "Typescript",
+        "Php",
+      ],
     },
     {
       name: "Back-end",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Porta at lectus leo eget condimentum dui aliquam. Amet nulla.",
-      images: ["img6", "img7", "img8", "img9", "img10"],
+        "Using tools like Node.js, MongoDB, and MySQL, I build robust back-end systems that power my full-stack web apps.",
+      images: ["NodeJs", "MongoDB", "MySQL", "Linux", "Github"],
     },
     {
       name: "Game Development",
       description:
-        "Lorem ipsum dolor sit amet consectetur. Porta at lectus leo eget condimentum dui aliquam. Amet nulla.",
-      images: ["img11", "img12", "img13", "img14", "img15"],
+        "With engines ranging from ZDoom to Unreal Engine 3/4 and even some unity, I've created and released several games on PC and Android with a focus on 3D graphics and gameplay.",
+      images: [
+        "Unreal",
+        "Adobe",
+        "Blender",
+        "Unity",
+        "Android",
+      ],
     },
   ];
 
@@ -44,6 +74,27 @@ export default function TechStack() {
     }
     return "text-2xl font-bold leading-8";
   };
+
+  const makeIconComponent = (img: string) => {
+    return {
+      NextJS: SiNextdotjs,
+      React: FaReact,
+      TailwindCSS: SiTailwindcss,
+      Typescript: SiTypescript,
+      Php: FaPhp,
+      NodeJs: FaNodeJs,
+      MongoDB: SiMongodb,
+      MySQL: GrMysql,
+      Linux: FaLinux,
+      Github: FaGithub,
+      Unreal: SiUnrealengine,
+      Adobe: SiAdobe,
+      Blender: SiBlender,
+      Unity: SiUnity,
+      Android: FaAndroid,
+    }[img];
+  }
+    
 
   const stackBlock = () => {
     return techStacks.map((stack) => (
@@ -82,12 +133,13 @@ export default function TechStack() {
         </div>
       </div>
       <div
-        className="flex justify-center gap-[8rem] mt-[6rem] max-md:flex-wrap"
+        className="flex justify-center gap-[8rem] mt-[6rem] max-md:flex-wrap text-6xl"
         key="GridDivOfTechStack"
       >
-        {currentStack.images.map((img) => (
-          <div key={img}>{img}</div>
-        ))}
+        {currentStack.images.map((img) => {
+          const IconComponent = makeIconComponent(img);
+          return IconComponent ? <IconComponent key={img} title={img} /> : null;
+        })}
       </div>
     </div>
   );
